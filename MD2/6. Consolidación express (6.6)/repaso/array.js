a4f9d83d8b1963be473241b8d566b7a731332b62
Carlos H <98904939+cherediachimeno@users.noreply.github.com>
2023-06-06 15:05:01 +0200

    repaso con cards

---
+++ MD2/6. Consolidación express (6.6)/repaso/array.js
@@ -0,0 +1,9 @@
+let array = [
+  { nombre: "Zelda", plataforma: "Nintendo", nota: "10" },
+  { nombre: "Mario", plataforma: "Nintendo", nota: 9 },
+  { nombre: "God of War", plataforma: "Sony", nota: 8 },
+  { nombre: "Metal of Gear", plataforma: "Sony", nota: 9 },
+  { nombre: "Resident Evil", plataforma: "Varias", nota: 8 },
+];
+
+module.exports = array;
--- MD2/6. Consolidación express (6.6)/repaso/index.js
+++ MD2/6. Consolidación express (6.6)/repaso/index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 
+const array = require("./array");
+
 app.get("/", (req, res) => {
   res.send("bienvenido a la ruta raíz");
 });
@@ -21,10 +23,50 @@ app.get("/probando/:name/:surname", (req, res) => {
 });
 
 app.get("/query", (req, res) => {
-  let nombre = req.query.nom;
-  let apellido = req.query.cognom;
+  let nombre = req.query.name;
+  let apellido = req.query.surname;
+  let curso = req.query.course;
+  let mail = req.query.mail;
+  let direccion = req.query.address;
 
-  res.send(`<h1>Encantado de volver a verte ${nombre} ${apellido}`);
+  res.send(`<h1>Encantado de volver a verte ${nombre} ${apellido}. 
+  Aquí te dejo el resto de información aportada</h1>
+  <ul>
+    <li>Curso: ${curso}</li> 
+    <li>Mail: ${mail}</li> 
+    <li>Dirección: ${direccion}</li> 
+  </ul>`);
+});
+
+app.get("/todo", (req, res) => {
+  let html = `<div style='display: flex; flex-wrap: wrap'>`;
+
+  for (let i = 0; i < array.length; i++) {
+    html += `
+    <div style='border: 2px black solid; margin: 5px; width: 30%'> 
+    <ul>
+    <li>
+    <strong>Título</strong>: ${array[i].nombre}
+    </li>
+    <li>
+    Plataforma: ${array[i].plataforma}
+    </li>
+    <li>
+    Nota: ${array[i].nota}
+    </li></div>`;
+  }
+  html += `</div>`;
+  res.send(html);
+});
+
+app.get("/sumar-juego", (req, res) => {
+  let titulo = req.query.titulo;
+  let plataforma = req.query.plataforma;
+  let nota = req.query.nota;
+
+  let objeto = { nombre: titulo, plataforma: plataforma, nota: nota };
+  array.push(objeto);
+  res.send("<h3>Juego guardado, vuelve a la ruta /todo");
 });
 
 app.listen(3000);
