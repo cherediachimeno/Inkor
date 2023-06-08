98610079c5e714df26ec5319dfb696c7a2cd51d4
Carlos H <98904939+cherediachimeno@users.noreply.github.com>
2023-06-08 14:00:05 +0200

    buscador

--- MD2/7. Metodos Express (8.6)/array.js
+++ MD2/7. Metodos Express (8.6)/array.js
@@ -1,25 +1,25 @@
 let especies = [
   {
-    especie: "Kel Dor",
-    mundo: "Star Wars",
+    especie: "Kel dor",
+    mundo: "star wars",
     imagen:
       "https://staticdelivery.nexusmods.com/mods/3061/images/thumbnails/220/220-1598200254-1898534520.png",
   },
   {
-    especie: "proteanos",
-    mundo: "Mass Effect",
+    especie: "Proteanos",
+    mundo: "mass effect",
     imagen:
-      "https://static.wikia.nocookie.net/masseffect/images/a/ae/Personaje_-_Javik.png",
+      "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2017/02/mass-effect-andromeda-nuevas-imagenes_3.jpg",
   },
   {
     especie: "Qunari",
-    mundo: "Dragon Age",
+    mundo: "dragon age",
     imagen:
       "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/03/Qunari.jpg",
   },
   {
     especie: "Draenei",
-    mundo: "Warcraft",
+    mundo: "warcraft",
     imagen:
       "https://images.blz-contentstack.com/v3/assets/blt3452e3b114fab0cd/blt0ca9cf0e194ad29c/616a007b19046c4b997493e2/KVRQFSLN28TG1605557309039.png",
   },
