0fa6d40252dbd7a8b26a0f3e206f6d3d12e990ae
Carlos H <98904939+cherediachimeno@users.noreply.github.com>
2023-09-07 14:29:11 +0200

    php y sql final crud

---
+++ MD3/17. Buscador (7.9)/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    * {
+        background-color: lightgrey;
+    }
+    </style>
+</head>
+<body>
+    
+<form action="search.php" method="post">
+Search <input type="text" name="buscador"><br>
+<input type ="submit">
+</form>
+
+
+</body>
+</html>
